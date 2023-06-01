00f154ca1edb50dcf774ad91e3d6f0ecd916959c
José Pablo <jose94pablo@gmail.com>
2023-05-31 18:07:11 -0600

    Python para Backend: web server con FastAPI

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 def run():
   '''
+  data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
